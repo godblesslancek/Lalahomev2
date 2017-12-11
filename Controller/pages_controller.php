c11f3c760fe2055620d34eb109e6fe41a268812b
thomasbuatois <thomas.buatois@gmail.com>
2017-12-11 15:14:09 +0100

    Added pages controller
    Action are defined, but the is no model yet

---
+++ Controller/pages_controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasbuatois
+ * Date: 25/11/2017
+ * Time: 14:50
+ */
+class PagesController {
+
+    public function home() {
+        //require_once('views/pages/home.php');
+    }
+
+    public function connection(){
+        //require_once 'views/pages/login.php';
+    }
+
+    public function error() {
+        //require_once('views/pages/error.php');
+    }
+
+    public function register_user(){
+        //require_once ('views/pages/create_user_form.php');
+    }
+}
+?>
