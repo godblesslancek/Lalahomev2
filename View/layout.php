cfcb53f83011863f610b4b2091c2903056e56dcc
thomasbuatois <thomas.buatois@gmail.com>
2017-12-11 15:08:56 +0100

    Added routeur for body

---
+++ View/layout.php
@@ -0,0 +1,22 @@
+<?php require_once ('Controller/routes_header.php');?>
+<!DOCTYPE html>
+    <html lang="fr">
+
+        <head>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="style.css" />
+            <title>TECHOUSE</title>
+        </head>
+
+        <header>
+            <?php require_once ('pages/header.php'); ?>
+        </header>
+        <body>
+            <?php require_once('Controller/routes.php'); ?>
+        </body>
+
+        <footer>
+        <?php require_once ('pages/footer.php')?>
+        </footer>
+
+    </html>
