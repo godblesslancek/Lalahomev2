30d1e647f6e6a79a87535f86dffa8202abb30481
thomasbuatois <thomas.buatois@gmail.com>
2017-12-19 09:18:33 +0100

    Revert "View changemement"
    This reverts commit ed6d045c1725e68cd7971d8e4208e2dc957e9755.

--- View/pages/login.php
+++ View/pages/login.php
@@ -1,11 +1,13 @@
 
-<div id="Connexion" >
-    <br>
+<div class="MonEspace" >
     <center>  <img class="espace" src="View/Content/images/connex.png" alt="espace" />
+</div> </center>
 
+<h1>Mon espace</h1>
 
-        <h1>Mon espace</h1>
+<div id="Connexion">
 
+    <center>
 
         <form enctype="multipart/form-data" method="post" accept-charset="UTF-8" action="index.php?controller=user&action=connect">
             <label for="Email">Adresse e-mail :</label>
