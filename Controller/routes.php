674c1646b980833f8962252a68a91bb2f936098c
thomasbuatois <thomas.buatois@gmail.com>
2017-12-22 13:22:08 +0100

    Added faq and messaging in pages for admin

--- Controller/routes.php
+++ Controller/routes.php
@@ -16,11 +16,9 @@
  if(isset($_SESSION['Role']) && !empty($_SESSION['Role'])){
      switch ($_SESSION['Role']):
          case 'admin':
-<<<<<<< HEAD
-             $controllers = array('pages' => ['home', 'error', 'login', 'register_user', 'home_user', 'stats', 'messages']);
-=======
-             $controllers = array('pages' => ['home', 'error', 'login', 'register_user', 'home_user', 'stats','faq','home_demo']);
->>>>>>> master
+             $controllers = array('pages' => ['home', 'error', 'login', 'register_user', 'home_user', 'stats', 'faq', 'messages']);
+
+
              break;
          case 'FU':
              $controllers = array('pages' => ['home', 'error', 'login', 'home_user','faq']);
