2c079d7e64ec9fbf1b18640f11fbf89b12ddaf1c
thomasbuatois <thomas.buatois@gmail.com>
2018-01-01 20:30:59 +0100

    Added helper and get conversation
    WORK IN PROGESS

--- Model/Message.php
+++ Model/Message.php
@@ -26,26 +26,52 @@ class Message
 
     public function add_message($message){
 
-        $stmt = $this->conn->prepare('INSERT INTO messages (id_sender, id_receiver, message) VALUES (?,?,?)');
-        $stmt->bind_param("iis",$this->ID_sender, $this->ID_receiver, $message);
+        $stmt = $this->conn->prepare('INSERT INTO messages (id_sender, id_receiver, message, datetime) VALUES (?,?,?,?)');
+        $date = (string)date('Y/m/d H:i:s');
+        $stmt->bind_param("iiss",$this->ID_sender, $this->ID_receiver, $message, $date);
         $stmt->execute();
         $stmt->close();
     }
 
     public function get_messages(){
-        $stmt = $this->conn->prepare('SELECT * FROM messages WHERE id_receiver = ?  AND id_sender = ?');
-        $stmt->bind_param("ii",$this->ID_sender, $this->ID_receiver);
+        $stmt = $this->conn->prepare('SELECT * FROM messages WHERE (id_receiver = ?  AND id_sender = ?) OR (id_sender = ? AND id_receiver = ?)  ORDER BY datetime ASC ');
+        $stmt->bind_param("iiii",$this->ID_receiver, $this->ID_sender, $this->ID_receiver, $this->ID_sender);
         $stmt->execute();
-        $row = $stmt->get_result();
+        $res = $stmt->get_result();
         $stmt->free_result();
         $stmt->close();
 
-        foreach ($row->fetch_assoc() as $message){
-            array();
+        $rows = array();
+        while ($row = $res->fetch_assoc()) {
+            $rows[] = $row;
         }
-
-
+        return $rows;
 
     }
+
+    public function get_conversations(){
+        $stmt = $this->conn->prepare('SELECT DISTINCT id_sender, id_receiver FROM messages WHERE id_receiver = ? ORDER BY datetime DESC');
+        $stmt->bind_param("i", $this->ID_receiver);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $stmt->free_result();
+        $stmt->close();
+
+        $rows = array();
+        while ($row = $res->fetch_assoc()) {
+            if ($row['id_sender'] != $this->ID_receiver)
+                $rows[] = $row['id_sender'];
+        }
+        return $rows;
+    }
+
+
+    //Helper for JS
+
+    public static function GetSession(){
+        return json_encode($_SESSION);
+    }
+
+
 
 }
