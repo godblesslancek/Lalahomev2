c64959f1024e771c98247718d082b3d158c9923b
thomasbuatois <thomas.buatois@gmail.com>
2018-01-01 20:30:01 +0100

    Added function for messagejs
    On page load get conversation
    When click on conversation get messages for that conversation

--- View/Content/js/messages.js
+++ View/Content/js/messages.js
@@ -1,25 +1,115 @@
-$('#btn_send_message').click(function(e){
+$( document ).ready(function() {
+    getconversation();
+});
 
+
+$('#btn_send_message').click(function(e){
     e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
 
     var message = encodeURIComponent( $('#Message').val() );
 
-    $('#btn_send_message').click(function(e){
-        e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
+    if(message != ""){ // on vérifie que les variables ne sont pas vides
+        $.ajax({
+            url : "index.php?controller=messages&action=send", // on donne l'URL du fichier de traitement
+            type : "POST", // la requête est de type POST
+            data : "Message=" + message,
+            dataType: 'json',
+            success: function (data) {
+                addmessage(true,message); // on ajoute le message dans la zone prévue
+            }
+        });
 
-        var message = encodeURIComponent( $('#Message').val() );
 
-        if(message != ""){ // on vérifie que les variables ne sont pas vides
-            $.ajax({
-                url : "index.php?controller=messages&action=send", // on donne l'URL du fichier de traitement
-                type : "POST", // la requête est de type POST
-                data : "Message=" + message // et on envoie nos données
+    }
+});
+
+function getmessage(IDConv) {
+    var IDUser = getSession('IDuser');
+    $.ajax({
+        url : "index.php", // on donne l'URL du fichier de traitement
+        type : "GET", // la requête est de type POST
+        data : "controller=messages&action=retreivemessage&IDonv=" + IDConv, // et on envoie nos données,
+        success: function (data) {
+
+            var messages = JSON.parse(data);
+            $.each(messages, function(index, obj){
+                if (IDUser == obj.id_sender)
+                    addmessage(true,obj.message, obj.datetime);
+                else
+                    addmessage(false,obj.message, obj.datetime);
             });
-
-            $('#messages').append("<p>" + message + "</p>"); // on ajoute le message dans la zone prévue
         }
     });
 
 
+}
 
-});
+function getconversation(){
+    $.ajax({
+        url : "index.php", // on donne l'URL du fichier de traitement
+        type : "GET", // la requête est de type POST
+        data : "controller=messages&action=retreiveconversation", // et on envoie nos données,
+        success: function (data) {
+            var converversations = JSON.parse(data);
+            $.each(converversations,function(index){
+                addconversation(converversations[index][0],converversations[index][1],converversations[index][2]);
+            })
+
+            $(".conversation").click(function(){
+                var IDUser = $(this).attr('iduser');
+                $('#messages').empty();
+                getmessage(IDUser);
+            });
+        }
+    });
+
+}
+
+function addmessage(sender, message, time){
+    if(!sender){
+        $('#messages').append('<div class="message darker">\n' +
+        '            <img src="View/Content/images/BM_avatar.png" alt="Avatar" class="right">\n' +
+        '            <p>' + message + '</p>\n' +
+            '<span class="time-left">' + time + '</span>' +
+        '        </div>');
+    }
+    else{
+        $('#messages').append('<div class="message">\n' +
+            '            <img src="View/Content/images/user_avatar.jpeg" alt="Avatar" class="right">\n' +
+            '            <p>' + message + '</p>\n' +
+            '<span class="time-right">' + time + '</span>' +
+            '        </div>');
+    }
+}
+
+function addconversation(display_name, role, iduser){
+
+    $('#panel-left').append('<div class="conversation" iduser="' + iduser+'">\n' +
+        '            <img src="View/Content/images/user_avatar.jpeg" alt="Avatar">\n' +
+        '            <p>' + display_name + '</p>\n' +
+        '        </div>');
+}
+//Deprecate
+
+// TODO
+// use a cookie parser
+function getSession(name){
+    var result;
+    $.ajax({
+        async : false,
+        url : "index.php", // on donne l'URL du fichier de traitement
+        type : "GET", // la requête est de type POST
+        data : "controller=messages&action=GetSession", // et on envoie nos données,
+        success: function (data) {
+            sessions = JSON.parse(data);
+            if (name in sessions){
+                result = sessions[name];
+            }
+            else
+                result =  null;
+        }
+    });
+
+    return result;
+
+}
