fc9ff96f6449c8aeeceea3f512459c181da0eb81
thomasbuatois <thomas.buatois@gmail.com>
2018-01-01 20:31:41 +0100

    Added function to get conversation and message
    WORK IN PROGESS

--- Controller/messages_controller.php
+++ Controller/messages_controller.php
@@ -28,10 +28,57 @@ class MessageController{
         }
     }
 
-    public function retreive(){
+    public function retreivemessage(){
         if(helper::checkSession(array('IDuser'))){
-            
+            $currentUser = new Users();
+            $currentUser->setCurrentUser($_SESSION['IDuser']);
 
+            switch ($currentUser->getRole()){
+                case 'admin':
+                    if(helper::checkGet(array('IDConv'))){
+                        $message = new Message($_SESSION['IDuser'],$currentUser->getIDBM());
+                        echo json_encode($message->get_messages());
+                    }
+
+                    break;
+                case 'FM':
+                    $message = new Message($_SESSION['IDuser'],$currentUser->getIDBM());
+                    echo json_encode($message->get_messages());
+                    break;
+                case 'BM':
+                    $message = new Message($_SESSION['IDuser'],$currentUser->getIDBM());
+                    echo json_encode($message->get_messages());
+                    break;
+            }
         }
+    }
+
+    public function retreiveconversation(){
+
+        if(helper::checkSession(array('IDuser'))){
+
+            $currentUser = new Users();
+            $currentUser->setCurrentUser($_SESSION['IDuser']);
+            $message = new Message($_SESSION['IDuser'],$currentUser->getIDBM());
+            $users= $message->get_conversations();
+            $conversations = array();
+
+            for ($i = 0; $i <= count($users) -1 ; $i++ ){
+                $cuser = new Users();
+                $cuser->setCurrentUser($users[$i]);
+                $cuser->getFirstName();
+                $conversations[$i] = array($cuser->getFirstName()." ".$cuser->getLastName(), $cuser->getRole(), $cuser->getID());
+
+            }
+
+            echo json_encode($conversations);
+        }
+
+    }
+
+    //Helper for JS
+
+    public function GetSession(){
+        echo json_encode($_SESSION);
     }
 }
