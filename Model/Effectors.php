e9680c12280096afea4da44ab5f03ab8a92e9a2b
thomasbuatois <thomas.buatois@gmail.com>
2018-01-08 09:08:43 +0100

    Refractor code database handler

--- Model/Effectors.php
+++ Model/Effectors.php
@@ -6,6 +6,8 @@ class Effector
     private $type_effector;
     private $id_room;
 
+    private $conn;
+
     
     public function getID()
     {
@@ -22,29 +24,28 @@ class Effector
         return $this->id_room;
     }
 
+    public function __construct()
+    {
+        $db = Database::getInstance();
+        $this->conn = $db->getConnection();
+    }
 
 
     public function create_effector($effector_param){
 
-        $db = Database::getInstance();
-        $conn = $db->getConnection();
-        $stmt = $conn->prepare('INSERT INTO effector (type_effector,id_room) VALUES (?,?)');
+        $stmt = $this->conn->prepare('INSERT INTO effector (type_effector,id_room) VALUES (?,?)');
         $stmt->bind_param("si", $effector_param['type_effector'],$effector_param['id_room'] );
         $stmt->execute();
         $stmt->close();
-        $conn->close();
     }
     
     public function update_effector($effector_param){
-        $db = Database::getInstance();
-        $conn = $db->getConnection();
-        $stmt = $conn->prepare('UPDATE effector SET type_effector = ?, id_room = ? WHERE id = ?')     ;
+
+        $stmt = $this->conn->prepare('UPDATE effector SET type_effector = ?, id_room = ? WHERE id = ?')     ;
         $stmt->bind_param("si", $effector_param['type_effector'],$effector_param['id_room'] );
         $stmt->execute();
         $stmt->close();
-        $conn->close();
-
-
+        
 
     }
 
