86413baf6b0efbea6149a8a7973e7e0aabb009f9
godblesslancek <lancek@hotmail.fr>
2018-01-19 14:22:08 +0100

    modèle effector effectirf

---
+++ Controller/effector_controller.php
@@ -0,0 +1,166 @@
+<?php
+
+class EffectorController{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function connect(){
+
+        $post_params = array('Email', 'Password');
+
+        if ($this->checkPost($post_params)){
+
+            $Email = $_POST['Email'];
+            $Password = $_POST['Password'];
+
+            $user = new Users();
+            $connect = $user->connect($Email, $Password);
+            if ($connect){
+                setcookie('IDuser', $user->getID(), time() + 24*3600, "/", null, false, false);
+                $_SESSION['Role'] = $user->getRole();
+                $_SESSION['FirstName'] = $user->getFirstName();
+                $_SESSION['IDuser'] = $user->getID();
+
+                header('Location: index.php?controller=pages&action=home_user');
+            }
+            else{
+                header('Location: index.php?controller=pages&action=login');
+            }
+        }
+        else
+            header('Location: index.php?controller=pages&action=login');
+    }
+
+    public function disconnect(){
+        session_destroy();
+        header('Location: index.php');
+    }
+
+    private function checkPost($Post_Array){
+
+        foreach ($Post_Array as $key){
+            if(!isset($_POST[$key]) && empty($_POST[$key])){
+                return False;
+            }
+        }
+        return True;
+    }
+
+    public function register(){
+
+        $post_params = array('LastName', 'FirstName', 'Email', 'Phone', 'Password', 'Password_Verif', 'Role');
+
+        if ( $this->checkPost($post_params)) {
+
+            if ($_POST['Password'] != $_POST['Password_Verif']) {
+                $error = 'Password does not match';
+
+                $controller = 'pages';
+                $action = 'register_user';
+
+            } else {
+
+                $user = new Users();
+
+                $user_params = [
+                    "LastName" => $_POST['LastName'],
+                    "FirstName" => $_POST['FirstName'],
+                    "Email" => $_POST['Email'],
+                    "Phone" => $_POST['Phone'],
+                    "Password" => $_POST['Password'],
+                    'Role' => $_POST['Role']
+
+                ];
+
+                $user->register_user($user_params);
+
+
+                $controller = 'pages';
+                $action = 'home';
+
+            }
+        }
+
+
+    }
+
+    public function update(){
+
+        $post_params = array('LastName', 'FirstName', 'Email', 'Phone', 'Password', 'Password_Verif', 'Role');
+
+        if ($this->checkPost($post_params)) {
+
+            if ($_POST['Password'] != $_POST['Password_Verif']) {
+                $error = 'Password does not match';
+
+                $controller = 'pages';
+                $action = 'update_user';
+
+            } else {
+
+
+                $user = new Users();
+
+                $user_params = [
+                    "LastName" => $_POST['LastName'],
+                    "FirstName" => $_POST['FirstName'],
+                    "Email" => $_POST['Email'],
+                    "Phone" => $_POST['Phone'],
+                    'ID' => $_POST['ID']
+
+                ];
+
+                $user->update_user($user_params);
+
+            }
+        $controller = 'pages';
+        $action = 'home';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- View/pages/test.php
+++ View/pages/test.php
@@ -4,7 +4,7 @@ echo 'Bonjour et bienvenue dans votre environnement de test.sfhjbqsfghj ';
 
 $monCapteurTest = new Effector();
 
-$monCapteurTest->create_effector(array("type_effector"=>'light',"id_room"=>2/*,"value_effector"=>1*/));
+$monCapteurTest->create_effector(array("type_effector"=>"light","id_room"=>2,"value_effector"=>1));
 
 echo $monCapteurTest->ID;
 
