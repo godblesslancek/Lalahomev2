a68d253078675b05f9043a81d982769814c4b7e4
MacPower <thomas.buatois@gmail.com>
2018-01-23 18:25:56 +0100

    footer + faq (#19)

--- View/pages/faq.php
+++ View/pages/faq.php
@@ -1,30 +1,62 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: thomasbuatois
- * Date: 21/12/2017
- * Time: 23:22
  */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="faq.css">
+    <title>FAQ : Aide & Conseils</title>
+</head>
+<body>
 
-?>
-    <h1> FAQ </h1>
-    <h5>Comment puis-je accéder à une vue d’ensemble de mes capteurs ? </h5>
-    <p>Vous pouvez accéder à vos équipements directement depuis le site Web, dans l’onglet « Capteurs ».</p><br/><br/>
-    <h5>Comment puis-je changer mon mot de passe ?</h5>
-    <p>Vous pouvez changer votre mot de passe directement depuis le site Web, dans l’onglet « Mon compte ».</p><br/><br/>
-    <h5>Je n’arrive pas à me connecter. Que puis-je faire ?</h5>
-    <p>Vous pouvez utiliser l’option « Mot de passe oublié » qui vous enverra un mail avec un nouveau mot de passe. Vous pourrez ensuite le changer via l’onglet « Mon compte ».</p><br/><br/>
-    <h5>Comment fonctionne le réglage de température de mon domicile ?</h5>
-    <p>Via l’onglet « Capteurs », vous pouvez consulter la température actuelle de votre domicile. A côté, deux boutons « + » et « - » vous permettent d’ajuster la température par tranche de 0,5°C.</p><br/><br/>
-    <h5>Que contient le système d’alarme ?</h5>
-    <p>À l'aide du système d'alarme, vous pouvez sécuriser rapidement et simplement votre domicile. À l'aide de différents détecteurs de mouvement, vous pouvez, selon le paramétrage, déclencher une alarme sonore.</p><br/><br/>
-    <h5>Comment gérer la consommation énergétique de mon habitation ?</h5>
-    <p>Dans votre espace personnel, vous disposez d’un onglet « Statistiques ». Vous y trouverez des graphiques de votre consommation énergétique mensuelle.</p><br/><br/>
-    <h5>Je suis administrateur. Comment gérer mes utilisateurs ?</h5>
-    <p>Dans votre espace personnel, vous disposez d’un onglet « Utilisateurs » qui vous permet de visualiser tous les utilisateurs des appartements que vous administrez. Vous pouvez  créer, modifier, ou supprimer un utilisateur.
-    </p><br/><br/>
-    <h5>Avec quels smartphones l’application SmartHouse peut-elle être pilotée ?</h5>
-    <p>A l’heure actuelle, l’application prend en charge les smartphones avec les systèmes d’exploitation Android ou iOS.</p><br/><br/>
-    <h5>Puis-je utiliser l’application SmartHouse en étant hors-ligne ?</h5>
-    <p>Pour l’instant, il n’est pas possible d’utiliser SmartHouse sans connexion. Cependant, TECHOUSE travaille aux côtés des meilleurs ingénieurs et consultants, afin de vous offrir un produit optimal et qui répond à vos attentes. </p><br/><br/>
+    <h4 id="titrefaq">Foire aux questions: aide et conseils</h4>
 
+    <section class="faq-section">
+
+    <input type="checkbox" id="q1">
+        <label for="q1">Comment puis-je accéder à une vue d’ensemble de mes capteurs ?</label>
+        <p>Vous pouvez accéder à vos équipements directement depuis le site Web, dans l’onglet « Capteurs ».</p>
+
+    <input type="checkbox" id="q2">
+        <label for="q2">Comment puis-je changer mon mot de passe ?</label>
+        <p>Vous pouvez changer votre mot de passe directement depuis le site Web, dans l'onglet « Mon compte ».</p>
+
+    <input type="checkbox" id="q3">
+        <label for="q3">Je n’arrive pas à me connecter. Que puis-je faire ?</label>
+        <p>Vous pouvez utiliser l’option « Mot de passe oublié » qui vous enverra un mail avec un nouveau mot de passe.[...]</p>
+        <p>Vous pourrez ensuite le changer via l’onglet « Mon compte ».</p>
+
+    <input type="checkbox" id="q4">
+        <label for="q4">Comment fonctionne le réglage de température de mon domicile ?</label>
+        <p>Via l’onglet « Capteurs », vous pouvez consulter la température actuelle de votre domicile. A côté, deux boutons[...]</p> 
+        <p>« + » et « - » vous permettent d’ajuster la température par tranche de 0,5°C.</p>
+
+     <input type="checkbox" id="q5">
+        <label for="q5">Que contient le système d’alarme ?</label>
+        <p>À l'aide du système d'alarme, vous pouvez sécuriser rapidement et simplement votre domicile. À l'aide de différents[...]</p>
+        <p>détecteurs de mouvement, vous pouvez, selon le paramétrage, déclencher une alarme sonore.</p>
+
+    <input type="checkbox" id="q6">
+        <label for="q6">Comment gérer la consommation énergétique de mon habitation ?</label>
+        <p>Dans votre espace personnel, vous disposez d’un onglet « Statistiques ». Vous y trouverez des graphiques de votre[...]</p>
+        <p>consommation énergétique mensuelle.</p>
+
+    <input type="checkbox" id="q7">
+        <label for="q7">Je suis administrateur. Comment gérer mes utilisateurs ?</label>
+        <p>Dans votre espace personnel, vous disposez d’un onglet « Utilisateurs » qui vous permet de visualiser tous les[...]</p> 
+        <p>utilisateurs des appartements que vous administrez. Vous pouvez  créer, modifier, ou supprimer un utilisateur.</p>
+
+    <input type="checkbox" id="q8">
+        <label for="q8">Avec quels smartphones l’application SmartHouse peut-elle être pilotée ?</label>
+        <p>A l’heure actuelle, l’application prend en charge les smartphones avec les systèmes d’exploitation Android ou iOS.</p>
+
+    <input type="checkbox" id="q9">
+        <label for="q9">Puis-je utiliser l’application SmartHouse en étant hors-ligne ?</label>
+        <p>Pour l’instant, il n’est pas possible d’utiliser SmartHouse sans connexion. Cependant, TECHOUSE travaille aux côtés[...]</p>
+        <p>des meilleurs ingénieurs et consultants, afin de vous offrir un produit optimal et qui répond à vos attentes.</p>
+
+
+    </section>
+
+
+</body>
+</html>
