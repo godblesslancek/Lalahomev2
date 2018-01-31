eae926b37865af45c1e1bc39b4456bde18feeb7a
ELYAAQOUBI <34472963+ELYAAQOUBI@users.noreply.github.com>
2018-01-31 15:29:26 +0100

    icone con/dec

--- View/pages/header.php
+++ View/pages/header.php
@@ -7,6 +7,7 @@
  */
 ?>
 <div id="header">
+
         <div class="logo" >
             <?php
                 if(isset($_SESSION['Role']) && !empty($_SESSION['Role'])){
@@ -20,7 +21,9 @@
             <img src="View/Content/images/logoTech.png" alt="Logo" />
         </div>
 
-        <div class="MonEspace" >
+
+
+        <div class="Connexion" >
             <?php
             if(isset($_SESSION['Role']) && !empty($_SESSION['Role']) && $_SESSION['Role']!= "FU"){
                 echo '<a href="index.php?controller=pages&action=messages">';
@@ -29,12 +32,16 @@
 
             if(isset($_SESSION['Role']) && !empty($_SESSION['Role'])){
                 echo '<a href="index.php?controller=user&action=disconnect">';
+                echo ' <img class="deconnexion" src="View/Content/images/deconnexion.png" alt="espace" /></a>';
             }
             else{
                 echo '<a href="index.php?controller=pages&action=login">';
+                echo '<img class="connexion" src="View/Content/images/connexion.png" alt="espace" /></a>';
             }
 
             ?>
-                <img class="espace" src="View/Content/images/connex.png" alt="espace" /></a>
+            
+
         </div>
+
 </div>
